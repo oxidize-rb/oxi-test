49b1b66eea1e479df7eb1bad398462c8efa55c11
Ian Ker-Seymer <hello@ianks.com>
2023-02-08 23:09:02 -0500

    Use modern rb-sys features (#8)
    * Use new rb-sys features
    
    * Upload artifact
    
    * Fix extconf

--- ext/src/lib.rs
+++ ext/src/lib.rs
@@ -1,3 +1,11 @@
+//! ⚠️ The `rb-sys` crate is a low level library. If you are looking to write a gem in
+//! Rust, you should probably use https://github.com/matsadler/magnus instead. It is powered
+//! by `rb-sys` and provides a higher level API, you get all of the
+//! cross-platform compatibility goodnees of `rb-sys`.
+//
+//! If you do need to drop down into raw libruby, you can enable the
+//! `rb-sys-interop` feature and add `rb-sys` to you Cargo dependencies.
+
 use rb_sys::{
     rb_define_module, rb_define_module_under, rb_define_singleton_method, rb_str_buf_append,
     rb_utf8_str_new_cstr, VALUE,
@@ -15,19 +23,12 @@ impl AsCStr for str {
     }
 }
 
-// ⚠️ The `rb-sys` crate is a low level library. If you are looking to write a gem in
-// Rust, you should probably use https://github.com/matsadler/magnus instead. It is powered
-// by `rb-sys` and provides a higher level API, you get all of the
-// cross-platform compatibility goodnees of `rb-sys`.
-//
-// If you do need to drop down into raw libruby, you can enable the
-// `rb-sys-interop` feature and add `rb-sys` to you Cargo dependencies.
 unsafe extern "C" fn hello(_: VALUE, name: VALUE) -> VALUE {
     rb_str_buf_append(rb_utf8_str_new_cstr("Hello, ".to_cstring()), name)
 }
 
 #[no_mangle]
-unsafe extern "C" fn Init_ext() {
+unsafe extern "C" fn Init_oxi_test() {
     let oxi_module = rb_define_module("Oxi".to_cstring());
     let oxi_test_module = rb_define_module_under(oxi_module, "Test".to_cstring());
 
@@ -43,7 +44,7 @@ unsafe extern "C" fn Init_ext() {
 
 #[cfg(test)]
 mod tests {
-    use crate::{AsCStr, Init_ext};
+    use crate::{AsCStr, Init_oxi_test};
     use std::os::raw::c_char;
 
     // By default, Cargo will run tests in parallel. This *will* segfault the
@@ -60,7 +61,7 @@ mod tests {
             rb_sys::ruby_sysinit(&mut argc, argv as _);
             rb_sys::ruby_init();
 
-            Init_ext();
+            Init_oxi_test();
 
             let mut result = rb_sys::rb_eval_string("Oxi::Test.hello('world')".to_cstring());
             let result = rb_sys::rb_string_value_cstr(&mut result);
